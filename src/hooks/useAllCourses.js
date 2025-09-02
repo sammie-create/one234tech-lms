49933a4c5f2cb6eaf88c98fb5255c4a28f8b2e89
Samuel Durumba <samuelrocks4real@gmail.com>
2025-09-02 09:03:10 +0100

    Updated useAllCourses hook

--- src/hooks/useAllCourses.js
+++ src/hooks/useAllCourses.js
@@ -8,11 +8,7 @@ function useAllCourses() {
     queryFn: async () => {
       const { data, error } = await supabase
         .from("courses")
-<<<<<<< HEAD
-        .select("id, title");
-=======
         .select("id, title, description, slug, total_modules, duration");
->>>>>>> one234tech-lms-second/main
 
       if (error) throw error;
       return data;
